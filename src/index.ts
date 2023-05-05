3df9b33216aee03271f01b169d4bdf716283042b
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2023-05-05 16:14:27 +0200

    Add source and test

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Require the package.json file as part of an import statement
+import * as packageJson from '../package.json';
+
+// A class that gets the version from the package.json file
+export class VersionUtil {
+  version: string;
+  constructor() {
+    // Check if the version is defined in the package.json file
+    if (!packageJson.version) {
+      throw new Error('Version not found in package.json');
+    }
+    this.version = packageJson.version;
+  }
+  getVersion() {
+    return this.version;
+  }
+}
